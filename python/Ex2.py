a9c389d4535f011beece65bcf722f17a808e727d
JanvRuij <jan.vanruijven96@gmail.com>
2023-12-16 16:25:11 +0100

    greedy2 is af

--- python/Ex2.py
+++ python/Ex2.py
@@ -20,7 +20,8 @@ class OnlineBPP:
         self.items = np.random.rand(self.m)
         self.generated += self.m
 
-    def greedy(self):
+    # keep adding until it doesnt fit anymore (sorted first)
+    def greedy1(self):
         self.items.sort()
         for item in self.items:
             # sort the indexes from high row sum to low
@@ -30,20 +31,50 @@ class OnlineBPP:
                 # check if the item fits
                 if np.sum(self.x[idx]) + item <= self.c:
                     # if it fits we append it
-                    non_zero_index = np.argmin(self.x[idx] != 0)
-                    self.x[idx][non_zero_index] = item
+                    zero_index = np.argmin(self.x[idx] == 0)
+                    self.x[idx][zero_index] = item
                     break
 
-        # if we havent generated n items we continou the process
+        # if we havent generated n items we continue the process
         if self.generated < self.n:
             self.generate_new_items()
-            self.greedy()
+            self.greedy1()
 
+        # otherwise we delete all empty bins and return
+        else:
+            self.x = self.x[~np.all(self.x == 0, axis=1)]
+            return
+
+    # divide items evenly over two bins (also sorted)
+    def greedy2(self):
+
+        # index 0 means we are dividing items over the first two bins
+        index = 0
+        self.items.sort()
+
+        for item_i in range(len(self.items)):
+            # for uneven we add to the first bin, otherwise the second bin
+            f_or_s = item_i % 2
+            zero_index = np.argmin(self.x[index] != 0)
+            if np.sum(self.x[index + f_or_s]) + self.items[item_i] <= self.c:
+                # if it fits we add the item to the bin
+                self.x[index][zero_index + f_or_s] = self.items[item_i]
+
+            # if it doesnt fit in both we update the index
+            else:
+                index += 1
+
+        # if we havent generated n items we continue the process
+        if self.generated < self.n:
+            self.generate_new_items()
+            self.greedy2()
+
+        # otherwise we delete all empty bins and return
         else:
             self.x = self.x[~np.all(self.x == 0, axis=1)]
             return
 
 
 x = OnlineBPP(100, 10)
-x.greedy()
+x.greedy2()
 print(x.x)
